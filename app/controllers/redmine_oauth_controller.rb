f200336c0b26a024771ca58290f1e748d9a5d022
kuma <kuma@p50.ap.dev.xkuma.xyz>
2021-08-23 21:21:57 +0800

    Fixed the problem of failing to save the setting of redirect uri in DB

--- app/controllers/redmine_oauth_controller.rb
+++ app/controllers/redmine_oauth_controller.rb
@@ -7,7 +7,7 @@ class RedmineOauthController < AccountController
   def oauth_google
     if Setting.plugin_redmine_omniauth_google[:oauth_authentification]
       session[:back_url] = params[:back_url]
-      redirect_to oauth_client.auth_code.authorize_url(:redirect_uri => settings[:Redirect_URI], :scope => scopes)
+      redirect_to oauth_client.auth_code.authorize_url(:redirect_uri => settings[:redirect_uri], :scope => scopes)
     else
       password_authentication
     end
@@ -18,7 +18,7 @@ class RedmineOauthController < AccountController
       flash[:error] = l(:notice_access_denied)
       redirect_to signin_path
     else
-      token = oauth_client.auth_code.get_token(params[:code], :redirect_uri => settings[:Redirect_URI])
+      token = oauth_client.auth_code.get_token(params[:code], :redirect_uri => settings[:redirect_uri])
       result = token.get('https://www.googleapis.com/oauth2/v1/userinfo')
       info = JSON.parse(result.body)
       if info && info["verified_email"]
--- init.rb
+++ init.rb
@@ -3,17 +3,17 @@ require_dependency 'redmine_omniauth_google/hooks'
 
 Redmine::Plugin.register :redmine_omniauth_google do
   name 'Redmine Omniauth Google plugin'
-  author 'Dmitry Kovalenok'
+  author 'Kuma Y.Y'
   description 'This is a plugin for Redmine registration through google'
-  version '0.0.1'
-  url 'https://github.com/twinslash/redmine_omniauth_google'
-  author_url 'http://twinslash.com'
+  version '0.0.2'
+  url 'https://github.com/kuma1416/redmine_omniauth_google'
+  author_url 'https://github.com/kuma1416/redmine_omniauth_google'
 
   settings :default => {
     :client_id => "",
     :client_secret => "",
     :oauth_autentification => false,
     :allowed_domains => "",
-    :Redirect_URI => ""
+    :redirect_URI => ""
   }, :partial => 'settings/google_settings'
 end
